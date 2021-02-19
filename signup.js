eec490109ba07108e36aef31452077f06a9f4989
rchoe1229 <ryan.choe@outlook.com>
2021-02-19 13:15:28 -0700

    Done v1.02

--- signup.js
+++ signup.js
@@ -4,21 +4,26 @@ const message = document.querySelector(".message")
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault()
   const formData = new FormData(event.target)
-  console.log(formData)
-  fetch("http://localhost:3000/login", {
+
+  fetch("http://localhost:3000/users/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify ({
-      name: formData.get("name"),
-      password: formData.get("password"),
+      user: {
+        name: formData.get("name"),
+        age: formData.get("age"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+      }
     })
   }).then(response => {
-    if (!response.ok) throw new Error("Not an OG User or Password 💩")
+    console.log(response)
+    if (!response.ok) throw new Error("Cannot Register User")
     return response.json()
   }).then(response => {
-    location.replace("http://localhost:3001/show-page.html")
+    location.replace("http://localhost:3001")
   }).catch(error => {
     message.textContent = error.message
   })
